653f80910613a15535a27db97bad8cf1c5995e83
angelayke <angebee2605@aol.com>
2022-09-06 22:22:18 +0200

    toujours pas trouver ou installer et comment utiliser le getUrlId

--- starwars/src/films/components/IndividualFilm.jsx
+++ starwars/src/films/components/IndividualFilm.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 //On importe le hook useParams de React Router
 import { useParams } from 'react-router-dom';
 import Col from "react-bootstrap/Col";
@@ -18,6 +18,7 @@ const peoplesService = new PeoplesService();
 const starshipsService = new StarshipsService();
 const vehiclesService = new VehiclesService();
 
+
 const IndividualFilm = () => {
     const [film, setFilm] = useState(null);
     const params = useParams();
@@ -27,7 +28,7 @@ const IndividualFilm = () => {
         film?.characters
     ); 
 
-    console.log(people);
+    console.log(getUrlId);
 
     const starship = useGetData(
         starshipsService.getStarshipsById.bind(starshipsService),
@@ -60,10 +61,25 @@ const IndividualFilm = () => {
     return (
        <>       
             <Row>
-                <h1>Film</h1>
                 <Col className='my-3'>
+                    <h1>Film</h1>
                     <h2 className='text-center'>{film.title}</h2>
                     <p className='text-center'>{film.opening_crawl}</p>
+
+                    <h2>Peoples in this film</h2>
+                    <Link to={`/film/${people.name}`}>
+                        <h3>{people.name}</h3>
+                    </Link>
+
+                    <h2>Vehicles in this film</h2>
+                    <Link to={`/film/${film.episode_id}`}>
+                        <h3>{vehicle.name}</h3>
+                    </Link>
+                    
+                    <h2>Starships in this film</h2>
+                    <Link to={`/film/${film.episode_id}`}>
+                        <h3>{starship.name}</h3>
+                    </Link>
                 </Col>
             </Row>
         </>
