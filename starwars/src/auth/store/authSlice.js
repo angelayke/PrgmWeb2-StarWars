fec5cd54503c4a71718ee3098519575eceeb876f
angelayke <angebee2605@aol.com>
2022-09-13 21:32:36 +0200

    mise en place de reducer dans authSlice

--- starwars/src/auth/store/authSlice.js
+++ starwars/src/auth/store/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import reducers from "./authReducers";
 
 export const authSlice =  createSlice({
         name: "auth",
@@ -9,8 +10,15 @@ export const authSlice =  createSlice({
                 id: ""
             },
         },
-        reducers: {}
+        reducers: reducers,
 });
+
+// On fait une destructuration de nos actions et on les exporte pour pouvoir les réutiliser dans toute notre application.
+export const {
+    updateEmail,
+    updateName,
+    updateWholeUser,
+    } = authSlice.actions;
 
 export default authSlice.reducer;
     
